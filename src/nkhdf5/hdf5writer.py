159d00b2247b11e6fa45d8585aa0de429cbf3be9
dastudillom <daniela.astudillo@ucsf.edu>
2025-04-14 10:38:30 -0700

    merge this version

--- src/nkhdf5/hdf5writer.py
+++ src/nkhdf5/hdf5writer.py
@@ -28,9 +28,9 @@ from edfreader import get_meastimestamp, edf_reader, normalize_dates
 # Main #
 if __name__ == "__main__":
     ## User-specified inputs
-    subject_id  = "PR06"
+    subject_id  = "PR04"
 
-    with open(f"/userdata/dastudillo/subjects.json", "r") as f: #stored in user's directory, not part of repo files
+    with open(f"/userdata/dastudillo/keys/subjects.json", "r") as f: #stored in user's directory, not part of repo files
         subjects = json.load(f)
     
     edf_dir = subjects[subject_id]["edf_dir"] #directory where raw EDF are stored
